be6c8aa57f6077af842a2d03f01f0ac1ed18fa77
god-like123 <32455256+god-like123@users.noreply.github.com>
2020-12-19 11:11:50 +0800

    Add files via upload

---
+++ componentLife/index.tsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './style.scss';
+import ReactDOM from 'react-dom';
+
+interface timerState {
+  date?: any;
+  timerID?: any;
+}
+export default class Clock extends Component<{}, timerState> {
+  // timerID: ReturnType<typeof setInterval> = null;  1.
+  // 2.
+  static timerID;
+
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount(): void {
+    // console.log('挂载成功');
+    // 2.
+    Clock.timerID = setInterval(() => this.tick(), 1000);
+    // 1.
+    // this.timerID = setInterval(() => this.tick(), 1000);
+    // console.log('挂载成功');
+  }
+
+  componentWillUnmount(): void {
+    clearInterval(Clock.timerID);
+    console.log('销毁');
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    const { date } = this.state;
+    return (
+      <div className="time-top-hello">
+        <h1>Hello,world!</h1>
+        <h2>It is {date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Clock />, document.getElementById('root'));
+// console.log(document.getElementById('root'));
